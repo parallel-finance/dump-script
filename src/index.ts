c1e82ae8a09c0a4b71aa353c6598b2565825a2e9
rjman <rjman.ljm@gmail.com>
2022-01-07 17:37:14 +0800

    Use lease period for vaultId

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,11 @@ import { LOCAL_ENPOINT, SUBSTRATE_SS58_PREFIX } from './utils/constants'
 dotenv.config()
 
 async function main() {
-  const { substrate, dumpPath, paraId } = getConfig()
+  const { substrate, dumpPath } = getConfig()
   const service = await Service.build({
     paraEndpoint: substrate.paraEndpoint || LOCAL_ENPOINT,
     relayEndpoint: undefined,
     dumpPath: dumpPath || './',
-    paraId: paraId || 2085,
     paraSS58Prefix: substrate.paraSS58Prefix || SUBSTRATE_SS58_PREFIX
   })
   await service.run()
